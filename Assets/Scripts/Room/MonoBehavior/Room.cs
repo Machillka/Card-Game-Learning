f160cc68d6bb26843b13e6c84e4999e053cc2a20
Machillka <machillka2007@gmail.com>
2025-03-29 17:49:42 +0800

    Add initial implementation of RoomType and RoomState enums, and update meta files for various assets

---
+++ Assets/Scripts/Room/MonoBehavior/Room.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public int column;
+    public int line;
+    private SpriteRenderer spriteRenderer;
+    public RoomDataSO roomData;
+    public RoomState roomState;
+    private void Awake()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        SetupRoom(0, 0, roomData);
+    }
+
+    /// <summary>
+    /// <description>
+    /// 定义点击房间行为，安卓触摸屏等点击同样适用
+    /// </summary>
+    private void OnMouseDown()
+    {
+        Debug.Log("点击了房间"+roomData.roomType);
+    }
+
+    /// <summary>
+    /// 外部创建房间时调用配置房间
+    /// </summary>
+    /// <param name="column"></param>
+    /// <param name="line"></param>
+    /// <param name="roomData"></param>
+    public void SetupRoom(int column, int line, RoomDataSO roomData)
+    {
+        this.column = column;
+        this.line = line;
+        this.roomData = roomData;
+        spriteRenderer.sprite = roomData.roomIcon;
+    }
+}
